e613f63611b0c4b2ec663fbb050e3a87b8c57d84
Vu1092001 <112168479+Vu1092001@users.noreply.github.com>
2022-09-23 15:57:07 -0700

    tjyhtg

---
+++ StringDataJPI/src/main/java/com/example/StringDataJPI/StringDataJpiApplication.java
@@ -0,0 +1,13 @@
+package com.example.StringDataJPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringDataJpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringDataJpiApplication.class, args);
+	}
+
+}
---
+++ StringDataJPI/src/test/java/com/example/StringDataJPI/StringDataJpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StringDataJPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringDataJpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
